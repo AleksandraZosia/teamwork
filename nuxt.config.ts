7b04e9c4153039c8678dc1ae064e088335fa21b8
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-12-11 12:24:26 +0100

    add nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
     css: ["@/assets/style/global.scss"],
+    modules: [
+        '@nuxt/image'
+    ]
 })
